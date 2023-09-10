98e331d901352d20787114927095845b5c478696
heybatmovlamov <heybetmovlamov2005@gmail.com>
2023-09-10 12:02:41 +0400

    initial commit

--- src/main/java/com/webperside/courseerpbackend/model/base/BaseResponse.java
+++ src/main/java/com/webperside/courseerpbackend/model/base/BaseResponse.java
@@ -1,2 +1,26 @@
-package com.webperside.courseerpbackend.model.base;public class BaseResponse {
+package com.webperside.courseerpbackend.model.base;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.net.http.HttpResponse;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BaseResponse<T> {
+    HttpStatus status;
+    String msg;
+    T data;
+    public static <T> BaseResponse <T> success(T data){
+        return BaseResponse.<T>builder().
+                status(HttpStatus.OK).
+                msg("Succes").
+                data(data).build();
+
+    }
 }
